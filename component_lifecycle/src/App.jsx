82898fa88a786cdc6e3dc19f1dc344021396def2
player-001 <mohitworksop@gmail.com>
2026-02-11 03:57:17 -0500

    added

---
+++ component_lifecycle/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+import Comp1 from "./Components/comp1"   //component name must start capital to get recognised as a compnent not as a HTML tag
+function App() {
+  return (
+    <>
+        <Comp1/>
+    </>
+  )
+}
+
+export default App
---
+++ component_lifecycle/src/Components/comp1.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+
+class comp1 extends Component{
+    constructor(){
+        super()
+
+    }
+    render(){
+        return (
+            <p>Hello</p>
+        )
+    }
+
+}
+export default comp1
