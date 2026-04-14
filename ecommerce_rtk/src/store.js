a798b7d5bb61d3e8b941efa8b992eb1aeb1e542a
player-001 <mohitworksop@gmail.com>
2026-04-14 17:39:11 -0400

    updated

--- ecommerce_rtk/src/store.js
+++ ecommerce_rtk/src/store.js
@@ -1,7 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit'
 import cartReducer from './Components/CartSlice'
 const store = configureStore({
-    reducer: {,
+    reducer: {
         cart: cartReducer,
     }
 })
