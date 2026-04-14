8fdfc87e7c4ad2d0fa1e88f8f55250324fc698ff
player-001 <mohitworksop@gmail.com>
2026-04-14 02:29:52 -0400

    added

--- custom_hook/src/Components/FetchYogaData.jsx
+++ custom_hook/src/Components/FetchYogaData.jsx
@@ -1,4 +1,28 @@
+import useFetch from "./UseFetch"
 const FetchYogaData = () => {
+    const [data] = useFetch('https://api.npoint.io/4459a9a10e43812e1152')
+    return (
+        <>
+        <h1>Yoga Benefits</h1>
+           
+        <ul className = "list_data_main">
+            {
+                data && data.map((item, index) => {
+                    const block = (
+                        <li key = {index} classsName = "list_data">
+                            <h3>{item.name}</h3>
+                            <p><strng>Benefits: </strng>{item.benefits}</p>
+                            <p><strong>Time Duration: </strong>{item.time_duration}</p>
+                        </li>
+                    )
+                    return block
+                })
+            }
+                
+        </ul>
+        
+        </>
 
-    return 
+    )
 }
+export default FetchYogaData
